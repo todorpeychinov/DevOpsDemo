5a7c7e98e1348e7ce1883083e0b57e2dae243b57
Todor Peychinov <161106514+todorpeychinov@users.noreply.github.com>
2026-01-15 10:53:41 +0200

    Added a new file

---
+++ example2.py
@@ -0,0 +1,23 @@
+def greet(name: str) -> str:
+    return f"Здравей, {name}! 👋"
+
+# this is function for adding two numbers
+def sum_numbers(a: float, b: float) -> float:
+    return a + b
+
+
+def main():
+    print("=== Примерна Python програма ===")
+    
+    name = input("Как се казваш? ")
+    print(greet(name))
+
+    x = float(input("Въведи число X: "))
+    y = float(input("Въведи число Y: "))
+
+    result = sum_numbers(x, y)
+    print(f"Сборът на {x} + {y} = {result}")
+
+
+if __name__ == "__main__":
+    main()
